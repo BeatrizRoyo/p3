d72ea33f202d1ede52003fb0ea0e13d568b946c7
BeatrizRoyo <beatrizroyo@alu.icai.comillas.edu>
2024-02-27 10:15:13 +0100

    ahora

--- afterQs.js
+++ afterQs.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Obtener el elemento donde quieres mostrar el número de respuestas correctas
     const respuestasCorrectasElement = document.getElementById('respuestas-correctas');
 
-    // Recuperar el número de respuestas correctas guardado en localStorage
     const respuestasCorrectas = localStorage.getItem('respuestasCorrectas');
 
     const mensajeCorrecto = document.getElementById('msj');
@@ -11,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Verificar si se recuperó correctamente el número de respuestas correctas
     if (respuestasCorrectas !== null) {
-        // Mostrar el número de respuestas correctas en el elemento correspondiente
         respuestasCorrectasElement.textContent = ` ${respuestasCorrectas}`;
         verifica30(respuestasCorrectas, mensajeCorrecto,btnBuscador);
         btnBuscador.addEventListener('click',function(){
@@ -20,7 +17,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     } else {
-        // En caso de que no se haya encontrado el número de respuestas correctas en localStorage
         respuestasCorrectasElement.textContent = 'No se encontraron respuestas correctas guardadas';
 
     }
@@ -30,7 +26,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function verifica30(nCAnswers,mensajeCorrecto,btnBuscador) {
     if (nCAnswers >= 3) {
-        // Código para mostrar alguna otra sección o realizar alguna acción
         mensajeCorrecto.textContent = 'Ya que has tenido mas de 4 aciertos, se acaba de desbloquear el buscador gratuito'
         btnBuscador.style.display = 'block';
 
